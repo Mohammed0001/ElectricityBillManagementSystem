a8fb2d8f9a38845f88c203c689effb0923531099
Bakr <maboubakr9@gmail.com>
2024-04-21 19:08:07 +0200

    Update ElectricityBillManaggementSytsem.java

--- ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
+++ ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
@@ -10,6 +10,6 @@ package mms.electricitybillmanaggementsytsem;
 public class ElectricityBillManaggementSytsem {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Test!!!");
     }
 }
