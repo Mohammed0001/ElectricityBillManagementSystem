05e8d1e03d87cf7cd1ae7fc6c1d18161905a8b07
nashwahossam <nashwaa.hossam211@gmail.com>
2024-04-21 20:54:36 +0200

    Employee

---
+++ ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/Employee.java
@@ -0,0 +1,28 @@
+
+package mms.electricitybillmanaggementsytsem;
+
+/**
+ *
+ * @author Nashwa 220488
+ */
+public class Employee implements ICategoryObserver{
+    
+    public String shift;
+    //Emloyee implements IElecTechReqR();
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    @Override
+    public void updateCategoryNotificstion(String str) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
