da0ccb506b1ddad1d8ceb7926e2a9270e89a39e6
nashwahossam <nashwaa.hossam211@gmail.com>
2024-04-23 18:31:54 +0200

    nashwaaa

--- ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/UtilityCompany.java
+++ ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/UtilityCompany.java
@@ -18,10 +18,15 @@ public class UtilityCompany extends User{
     // private ArrayList<ISaleReportRO> SaleReport = new ArrayList<>();
 
     public ArrayList<Integer> getContactNumber() {
+        
         return contactNumber;
     }
 
     public void setContactNumber(ArrayList<Integer> contactNumber) {
         this.contactNumber = contactNumber;
     }
+    
+    
+   
+    
 }
