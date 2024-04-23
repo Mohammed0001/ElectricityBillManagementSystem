fd11ae6adc81b1bb1878d564c297a2a47e39af07
Bakr <maboubakr9@gmail.com>
2024-04-23 16:32:29 +0200

    bakr

--- ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
+++ ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
@@ -16,24 +16,9 @@ import java.util.logging.Logger;
 public class ElectricityBillManaggementSytsem {
     
     public static void main(String[] args) {
-       new login().setVisible(true);
-
+        new login().setVisible(true);
         Database.getInsatnce() ;
-//        User u = new User();
-//        u.setName("Karim");
-//        u.setPassword("KarimPass");
-//        u.setUsername("karimm");
-//        System.out.println(u.createAccount());
-        ResultSet rs = Database.getInsatnce().selectStmt("SELECT * FROM users;");
-        try {
-            while(rs.next()){
-                
-                System.out.println(rs.getString("name")+" id : " + rs.getInt("id"));
-            }
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(ElectricityBillManaggementSytsem.class.getName()).log(Level.SEVERE, null, ex);
-        }
         System.out.println("Susscess!! in mms.electricitybillmanaggementsytsem.ElectricityBillManaggementSytsem.main()");
     }
+    
 }
