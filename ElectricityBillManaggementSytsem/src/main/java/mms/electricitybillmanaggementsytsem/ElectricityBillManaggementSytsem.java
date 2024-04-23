de2c8ad05e42ae52529d9d0f4930649f35d44c85
Bakr <maboubakr9@gmail.com>
2024-04-23 22:48:58 +0200

    Update ElectricityBillManaggementSytsem.java

--- ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
+++ ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
@@ -19,50 +19,13 @@ import java.util.logging.Logger;
 public class ElectricityBillManaggementSytsem {
     
     public static void main(String[] args) {
-<<<<<<< HEAD
+
        new login().setVisible(true);
-       
-//              BankCardForm bankCardForm = new BankCardForm();
-//     bankCardForm.setVisible(true);
-
-// InstaPayform  InstaPayform = new InstaPayform ();
-//        InstaPayform .setVisible(true);
-
 
         Database.getInsatnce() ;
-//        User u = new User();
-//        u.setName("Karim");
-//        u.setPassword("KarimPass");
-//        u.setUsername("karimm");
-
-
-//        System.out.println(u.createAccount());
-        ResultSet rs = Database.getInsatnce().selectStmt("SELECT * FROM users;");
-        try {
-            while(rs.next()){
-                
-                System.out.println(rs.getString("name")+" id : " + rs.getInt("id"));
-            }
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(ElectricityBillManaggementSytsem.class.getName()).log(Level.SEVERE, null, ex);
-        }
         System.out.println("Susscess!! in mms.electricitybillmanaggementsytsem.ElectricityBillManaggementSytsem.main()");
     }
-    
-    
-    
-    
-    
-    
  
-=======
-        new login().setVisible(true);
-        Database.getInsatnce() ;
-        System.out.println("Susscess!! in mms.electricitybillmanaggementsytsem.ElectricityBillManaggementSytsem.main()");
-    }
-    
->>>>>>> cb459f0dc2592abd3f72960dd08769c1c3a1715e
 }
 
 
