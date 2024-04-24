b85894b8a4c917eaed195d003dd72d9e72d30916
Bakr <maboubakr9@gmail.com>
2024-04-24 12:27:57 +0200

    Update ElectricityBillManaggementSytsem.java

--- ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
+++ ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
@@ -2,23 +2,21 @@
 
 package mms.electricitybillmanaggementsytsem;
 
-
-
 /**
  *
  * @author Electricity BIll Management System 
  * G#1
+ * 
  */
+
+
 public class ElectricityBillManaggementSytsem {
     
     public static void main(String[] args) {
+       new login().setVisible(true);
+       Database.getInsatnce() ;
+       System.out.println("Susscess!! in mms.electricitybillmanaggementsytsem.ElectricityBillManaggementSytsem.main()");
 
-     new login().setVisible(true);
-
-  
-
-        Database.getInsatnce() ;
-        System.out.println("Susscess!! in mms.electricitybillmanaggementsytsem.ElectricityBillManaggementSytsem.main()");
     }
  
 }
