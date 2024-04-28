11df690508d96d176f0b166375c3f31976a05ae8
Bakr <maboubakr9@gmail.com>
2024-04-28 23:21:36 +0300

    bbb

--- ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
+++ ElectricityBillManaggementSytsem/src/main/java/mms/electricitybillmanaggementsytsem/ElectricityBillManaggementSytsem.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 public class ElectricityBillManaggementSytsem {
     
     public static void main(String[] args) {
+        
         new login().setVisible(true);
         Database.getInsatnce() ;
         System.out.println("Susscess!! in mms.electricitybillmanaggementsytsem.ElectricityBillManaggementSytsem.main()");
